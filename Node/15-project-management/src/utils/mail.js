fc0afc5855e21ca7308b9459c283885fe47d835d
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2026-04-03 18:33:45 +0530

    Complete send email methods

---
+++ Node/15-project-management/src/utils/mail.js
@@ -0,0 +1,84 @@
+import Mailgen from "mailgen";
+import nodemailer from "nodemailer";
+
+const sendEmail = async (options) => {
+  const mailGenerator = new Mailgen({
+    theme: "default",
+    product: {
+      name: "Project Manager",
+      link: "https://projectmanagerlink.com",
+    },
+  });
+
+  const emailTextual = mailGenerator.generatePlaintext(options.mailgenContent);
+  const emailHtml = mailGenerator.generate(options.mailgenContent);
+
+  const transporter = nodemailer.createTransport({
+    host: process.env.MAILTRAP_SMTP_HOST,
+    post: process.env.MAILTRAP_SMTP_PORT,
+    auth: {
+      user: MAILTRAP_SMTP_USER,
+      pass: MAILTRAP_SMTP_PASS,
+    },
+  });
+
+  const mail = {
+    from: "mail.projectmanager@example.com",
+    to: options.email,
+    subject: options.subject,
+    text: emailTextual,
+    html: emailHtml,
+  };
+
+  try {
+    await transporter.sendMail(mail);
+  } catch (error) {
+    console.error("Email service failed: ", error);
+  }
+};
+
+const emailVerificationmailgenContent = (username, verificationUrl) => {
+  return {
+    body: {
+      name: username,
+      intro: "Welcome to our app! We are excited to have you on board",
+      action: {
+        instructions:
+          "To verify your email please click on the following button",
+        button: {
+          color: "#1ae5ff",
+          text: "Verify your email",
+          link: verificationUrl,
+        },
+      },
+      outro:
+        "Need help, or have questions? Just reply to this email, we'd love to help",
+    },
+  };
+};
+
+const forgotPasswordmailgenContent = (username, passwordResetUrl) => {
+  return {
+    body: {
+      name: username,
+      intro: "We got a request to reset the password of your account",
+      action: {
+        instructions:
+          "To reset your password please click on the following button or link",
+        button: {
+          color: "#22bc66",
+          text: "Reset Password",
+          link: passwordResetUrl,
+        },
+      },
+      outro:
+        "Need help, or have questions? Just reply to this email, we'd love to help",
+    },
+  };
+};
+
+export {
+  emailVerificationmailgenContent,
+  forgotPasswordmailgenContent,
+  sendEmail,
+};
